053fdf2bbf7cd9ab9c7e50917c2ae471712730ad
Michael Janks <mjanks@emich.edu>
2020-05-29 18:31:51 -0400

    added TFIDF calculator, IDF needs work

--- Text Summarizer/src/textSummarizer/Main.java
+++ Text Summarizer/src/textSummarizer/Main.java
@@ -2,6 +2,8 @@ package textSummarizer;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -9,7 +11,9 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		ArrayList<String> stopWordList = new ArrayList<String>();
 		ArrayList<String> wordListStopsRemoved = new ArrayList<String>();
-	    File file = new File("test.txt"); 
+		List<String> list = null;
+		List<List<String>> list2 = null;
+	    File file = new File("easywords.txt"); 
 	    File file2 = new File("stopwords.txt");
 	    Scanner scan = new Scanner(file);
 	    Scanner scan2 = new Scanner(file2);
@@ -24,13 +28,22 @@ public class Main {
 	    String[] tokens = str.split(delims);
 	    for (int i = 0; i < tokens.length; i++) {
 	        System.out.println("Token " + i + ": " + tokens[i]);
+	        list = Arrays.asList(tokens);
+	        list2 = Arrays.asList(list);
 	    }
+	    System.out.println("list2: " + list2);
 	    
 	    while(scan2.hasNext()) {
 	    	stopWordList.add(scan2.next().toLowerCase());
 	    }
 	    
 	    wordListStopsRemoved = s.removeStopWords(tokens[0].toString().toLowerCase(), stopWordList);
+	    // s.countFrequencies(wordListStopsRemoved);
+	    
+	    TFIDFCalculator calc = new TFIDFCalculator();
+	    String term = "stop";
+	    System.out.println(calc.tf(wordListStopsRemoved, term));
+	    System.out.println(calc.idf(list2, term));
 	    
 	    System.out.println();
 	    System.out.print("After stops removed: ");
--- Text Summarizer/src/textSummarizer/Summarizer.java
+++ Text Summarizer/src/textSummarizer/Summarizer.java
@@ -1,6 +1,8 @@
 package textSummarizer;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Summarizer {
 
@@ -21,5 +23,23 @@ public class Summarizer {
 		}
 		return newList;
 	}
+	
+	public static void countFrequencies(ArrayList<String> list) 
+    { 
+        // hashmap to store the frequency of element 
+        Map<String, Integer> hm = new HashMap<String, Integer>(); 
+  
+        for (String i : list) { 
+            Integer j = hm.get(i); 
+            hm.put(i, (j == null) ? 1 : j + 1); 
+        } 
+  
+        // displaying the occurrence of elements in the arraylist 
+        for (Map.Entry<String, Integer> val : hm.entrySet()) { 
+            System.out.println("Element " + val.getKey() + " "
+                               + "occurs"
+                               + ": " + val.getValue() + " times"); 
+        } 
+    } 
 
 }
